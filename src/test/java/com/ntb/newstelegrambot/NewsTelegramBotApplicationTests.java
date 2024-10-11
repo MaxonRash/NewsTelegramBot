ea5976e9de105535bd4ab7338559bfd85df5ab37
MaxonRash <rashnikov.m@yandex.com>
2024-10-11 14:48:55 +0300

    Initial commit

---
+++ src/test/java/com/ntb/newstelegrambot/NewsTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntb.newstelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
